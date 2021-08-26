e4ff16c606a616e4f21088fa980ee216d7ade19b
Leslie-Luo <dktluo@gmail.com>
2021-08-26 11:36:09 +0800

    feat: ✨ 添加代码依赖分析工具 rollup-plugin-visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import visualizer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // 要用到的插件数组
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   // 开发或生产环境服务的公共基础路径,可以是/foo/、https://foo.com/、空字符串或./(用于开发环境) 几种类型，这个选项也可以通过命令行参数指定（例：vite build --base=/my/public/path/）
   base: './',
   // 静态资源服务的文件夹, 默认"public"
